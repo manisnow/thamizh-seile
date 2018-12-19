c38686fa72e02d48d8ec70603d262233a38bd65c
manisnow <manisnow@gamil.com>
2018-12-19 07:12:37 -0500

    added login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-
-
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {MatDialog} from '@angular/material'
 @Component({
-  selector: 'app-root',
-  templateUrl: 'login.component.html',
-
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
- title ="உள் நுழை"
-  
-}
+export class LoginComponent implements OnInit {
+  constructor(private router: Router) { }
+  title ="உள் நுழை";
+username: string;
+password: string;
+  ngOnInit() {
+  }
+  login() : void {
+    if(this.username == 'admin' && this.password == 'admin'){
+     this.router.navigate(["user"]);
+    }else {
+      alert("Invalid credentials");
+    }
+  }
+  }
